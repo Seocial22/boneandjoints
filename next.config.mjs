7fe789f8f9629a814b1668a6fef88b5b29476e2d
root <root@srv1375773.hstgr.cloud>
2026-07-07 09:55:14 +0000

    Redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  async redirects() {
+    return [
+      {
+        source: "/blog/arthroscopy-surgery-ajmer-joint-pain-treatment",
+        destination:
+          "/blog/best-arthroscopy-surgery-ajmer-joint-pain-treatment",
+        permanent: true, // 301 Redirect
+      },
+    ];
+  },
 };
 
 export default nextConfig;
