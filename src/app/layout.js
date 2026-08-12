b98ffe338b3b7e18b79a4a6a8afe0733beea2426
root <root@srv1375773.hstgr.cloud>
2026-08-12 06:20:16 +0000

    Add Blog

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,6 +8,15 @@ import FirstVisitPopup from '@/components/FirstVisitPopup';
 
 
 export const metadata = {
+  robots: {
+    index: false,
+    follow: false,
+    nocache: true,
+    googleBot: {
+      index: false,
+      follow: false,
+    },
+  },
  metadataBase: new URL('https://boneandjoints.in'),
  title: 'Dr. Abhishek Saxena - Orthopedic Surgeon In Ajmer',
  description: 'Dr. Abhishek Saxena is a leading orthopedic surgeon in Ajmer providing advanced bone and joint treatments. Visit Our Clinic for expert orthopedic care, joint replacements, and sports injury treatments.',
@@ -17,6 +26,9 @@ export default function RootLayout({ children }) {
  return (
    <html lang="en">
      <head>
+        <meta name="robots" content="noindex, nofollow, noarchive" />
+        
+        
        <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,400;0,600;0,700;1,400&family=DM+Sans:wght@300;400;500;600&display=swap" rel="stylesheet" />
        <Script
          src="https://www.googletagmanager.com/gtag/js?id=G-4DNZQ0ML0V"
